55c01cb6040ed9ef30cef980f148702f2d7058fc
tranchiencongtd <tranchiencongtd@gmail.com>
2023-05-14 15:49:11 +0700

    init

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'Ecommerce',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44388/',
+    redirectUri: baseUrl,
+    clientId: 'Ecommerce_App',
+    responseType: 'code',
+    scope: 'offline_access Ecommerce',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44395',
+      rootNamespace: 'Ecommerce',
+    },
+  },
+} as Environment;
