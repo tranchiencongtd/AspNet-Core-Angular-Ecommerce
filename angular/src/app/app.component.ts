86d31e026bd4f3b8bef26538fb735968c5b5eaee
congtcdev <congtc.dev@gmail.com>
2023-12-24 17:39:52 +0700

    5.5. [FE] Hiển thị thông tin và đăng xuất

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,9 @@
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
+import { LOGIN_URL } from './shared/constants/urls.const';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +15,14 @@ export class AppComponent {
   menuMode = 'static';
 
   constructor(private primengConfig: PrimeNGConfig,
-    ) {}
+    private authService: AuthService,
+    private router: Router) {}
 
   ngOnInit() {
     this.primengConfig.ripple = true;
     document.documentElement.style.fontSize = '14px';
-    
+    if(this.authService.isAuthenticated() == false){
+      this.router.navigate([LOGIN_URL])
+    }
   }
 }
--- angular/src/app/layout/app.topbar.component.ts
+++ angular/src/app/layout/app.topbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { LOGIN_URL } from '../shared/constants/urls.const';
 
 @Component({
     selector: 'app-topbar',
@@ -9,6 +12,7 @@ import { LayoutService } from "./service/app.layout.service";
 export class AppTopBarComponent {
 
     items!: MenuItem[];
+    userMenuItems: MenuItem[];
 
     @ViewChild('menubutton') menuButton!: ElementRef;
 
@@ -16,5 +20,27 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService, private authService: AuthService, private router: Router) {}
+    ngOnInit(): void {
+      this.userMenuItems = [
+        {
+          label: 'Thông tin cá nhân',
+          icon: 'pi pi-id-card',
+          routerLink: ['/profile'],
+        },
+        {
+          label: 'Đổi mật khẩu',
+          icon: 'pi pi-key',
+          routerLink: ['/change-password'],
+        },
+        {
+          label: 'Đăng xuất',
+          icon: 'pi pi-sign-out',
+          command: event => {
+            this.authService.logout();
+            this.router.navigate([LOGIN_URL]);
+          },
+        },
+      ];
+    }
 }
