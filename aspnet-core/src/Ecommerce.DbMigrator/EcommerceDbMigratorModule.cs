a63ec5d324b433f47ef40bc906c4419bbd0787f2
congtcdev <congtc.dev@gmail.com>
2024-01-03 23:15:32 +0700

    update 8.0

--- aspnet-core/src/Ecommerce.DbMigrator/EcommerceDbMigratorModule.cs
+++ aspnet-core/src/Ecommerce.DbMigrator/EcommerceDbMigratorModule.cs
@@ -1,15 +1,20 @@
 ﻿using Ecommerce.EntityFrameworkCore;
 using Volo.Abp.Autofac;
+using Volo.Abp.Caching;
 using Volo.Abp.Modularity;
-
+using Volo.Abp.Caching.StackExchangeRedis;
 namespace Ecommerce.DbMigrator;
 
 [DependsOn(
     typeof(AbpAutofacModule),
+    // typeof(AbpCachingStackExchangeRedisModule),
     typeof(EcommerceEntityFrameworkCoreModule),
     typeof(EcommerceApplicationContractsModule)
     )]
 public class EcommerceDbMigratorModule : AbpModule
 {
-
+    // public override void ConfigureServices(ServiceConfigurationContext context)
+    // {
+    //     Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "Ecommerce:"; });
+    // }
 }
